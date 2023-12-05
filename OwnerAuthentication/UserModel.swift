d60760ac4403630b86c251c77bf8d2f46b91a1d2
keerthiv <keerthi@sparkouttech.com>
2023-12-05 18:32:50 +0530

    user model added

---
+++ OwnerAuthentication/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserModel.swift
+//  OwnerAuthentication
+//
+//  Created by apple on 05/12/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import Foundation
+
+struct UserModel {
+    var firstName: String?
+}
